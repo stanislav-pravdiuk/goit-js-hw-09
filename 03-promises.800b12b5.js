1969833986887e2b97e1e568e6566060b24ff4b1
stanislav-pravdiuk <114864904+stanislav-pravdiuk@users.noreply.github.com>
2023-03-09 07:57:48 +0000

    Deploying to gh-pages from @ stanislav-pravdiuk/goit-js-hw-09@43be08536cdac4162b86b2839e9304e6c3bcac77 🚀

---
+++ 03-promises.800b12b5.js
@@ -0,0 +1,2 @@
+!function(){var e={firstDelay:document.querySelector("button")};console.log(e.firstDelay)}();
+//# sourceMappingURL=03-promises.800b12b5.js.map
---
+++ 03-promises.9eef018c.js
@@ -0,0 +1,2 @@
+const e={firstDelay:document.querySelector("button")};console.log(e.firstDelay);
+//# sourceMappingURL=03-promises.9eef018c.js.map
